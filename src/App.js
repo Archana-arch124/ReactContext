9e16913d47adddcb72699b5834df83b265412197
Archana-arch124 <archgowda124@gmail.com>
2021-05-10 12:26:41 +0530

    [Archana]Added:ReactJs using Context in app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+// first we will make a new context
+const MyContext = React.createContext();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Then create a provider Component
+class MyProvider extends Component {
+  state = {
+    name: 'Archana Raj Gowda',
+    age: 20,
+    education: 'BE,CSE',
+    techSkills: 'Java,JS,HTML,CSS,ReactJS',
+    location: 'Bangalore'
+  }
+  render() {
+    return (
+      <MyContext.Provider value={{
+        state: this.state,
+        growAYearOlder: () => this.setState({
+          age: this.state.age + 1
+        })
+      }}>
+        {this.props.children}
+      </MyContext.Provider>
+    )
+  }
 }
+
+const Family = () => (
+  <div className="family">
+    <Person />
+  </div>
+)
+
+class Person extends Component {
+  render() {
+    return (
+      <div className="person">
+        <MyContext.Consumer>
+          {(context) => (
+            <React.Fragment>
+              <p>Name: {context.state.name}</p>
+              <p>Age: {context.state.age}</p>
+              <p>Education: {context.state.education}</p>
+              <p>TechSkills: {context.state.techSkills}</p>
+              <p>Location: {context.state.location}</p>
+              <button onClick={context.growAYearOlder} style={{ backgroundColor: 'pink' }}>BIRTHDAY</button>
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>
+      </div>
+    )
+  }
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <MyProvider>
+        <div className="main">
+          <div className="data">
+            <b>*DETAILS OF ARCHANA*</b>
+            <Family />
+          </div>
+        </div>
+      </MyProvider>
+    );
+  }
+}
+
 
 export default App;
